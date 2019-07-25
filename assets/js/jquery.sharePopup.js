8aa071b0c0378707b4e23c238d435ffcf0184d19
johndimen <davejohnlucero@gmail.com>
2019-07-26 01:49:06 +0800

    adding files

---
+++ assets/js/jquery.sharePopup.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+    jQuery(document).ready(function($) {
+   $('.share').click(function() {
+     var NWin = window.open($(this).prop('href'), '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
+     if (window.focus)
+     {
+       NWin.focus();
+     }
+     return false;
+    });
+});
+</script>

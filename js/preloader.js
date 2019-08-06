52e22add1f25d01f2c19c3cbee7658e03e05892d
johndimen <davejohnlucero@gmail.com>
2019-08-07 01:28:28 +0800

    added new files

---
+++ js/preloader.js
@@ -0,0 +1,8 @@
+(function ($){
+                /* Preloader Js
+    			===================*/
+    			$(window).on("load", function () {
+       		 	$('.preloader').fadeOut(2000);
+    			});
+})(jQuery);
+                
